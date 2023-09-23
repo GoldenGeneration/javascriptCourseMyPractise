c17298929945b8378dd534cd54e80ed598efa260
GoldApparition <goldapparition09@gmail.com>
2023-09-23 14:08:32 +0545

    First commit... Var,Const,let...ForEach,ForIn

---
+++ crashCouse.js
@@ -0,0 +1,9 @@
+// 1. keyword
+// 2. var const let
+// 3. hoisting
+// 4. types in js ( primitive and reference )
+//     1. primitives = numbers
+//     2. reference = [ ] ( ) { }
+// 5. 
+
+var a = 12;
